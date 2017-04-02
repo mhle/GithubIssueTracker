728a784c0fc0c13ff5d3f11e970247e46d7dad7a
Michael Le <hle.michael@gmail.com>
2017-04-02 23:49:15 +0100

    remove debug and simplify code

--- IssueViewer/IssueTrackerModel.swift
+++ IssueViewer/IssueTrackerModel.swift
@@ -21,13 +21,10 @@ struct IssueTrackerModel {
     return repositoryName
       .observeOn(MainScheduler.instance)
       .flatMapLatest { name -> Observable<Repository?> in
-        print("Name: \(name)")
-        return self.findRepository(withName: name)
+        self.findRepository(withName: name)
       }
       .flatMapLatest { repository -> Observable<[Issue]?> in
         guard let repository = repository else { return Observable.just(nil) }
-        
-        print("Repository: \(repository.fullName)")
         return self.findIssues(for: repository)
       }
       .replaceNilWith([])
@@ -36,14 +33,12 @@ struct IssueTrackerModel {
   internal func findIssues(for repository: Repository) -> Observable<[Issue]?> {
     return provider
       .request(Github.issues(repositoryFullname: repository.fullName))
-      .debug()
       .mapArrayOptional(type: Issue.self)
   }
   
   internal func findRepository(withName name: String) -> Observable<Repository?> {
     return provider
       .request(Github.repo(fullName: name))
-      .debug()
       .mapObjectOptional(type: Repository.self)
   }
 }
